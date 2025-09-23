e820ee80917d0bcf30ccddf9990f368ed2ad1235
Ahmad <aa2997168@gmail.com>
2025-09-23 15:31:32 +0300

    db models created

---
+++ backend/src/config/env.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const ENV = {
+    PORT:process.env.PORT,
+    NODE_ENV:process.env.NODE_ENV,
+    MONGO_URI:process.env.MONGO_URI,
+    CLERK_PUBLISHABLE_KEY:process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY:process.env.CLERK_SECRET_KEY,
+    ARCJET_KEY:process.env.ARCJET_KEY,
+    IMAGEKIT_PUBLIC_KEY:process.env.IMAGEKIT_PUBLIC_KEY,
+    IMAGEKIT_PRIVATE_KEY:process.env.IMAGEKIT_PRIVATE_KEY,
+    IMAGEKIT_URL_ENDPOINT:process.env.IMAGEKIT_URL_ENDPOINT,
+}
